3220c6b6f3e5799c2d0997b5a3d94f8bc3cb9793
baudot <pierre.baudot@gmail.com>
2020-06-10 23:42:31 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup, find_packages
- 
+
+import setuptools
+
 with open("README.md", "r") as fh:
-    long_description = fh.read() 
- 
-setup(name='infotopo',
- 
-      version='0.1',
- 
-      url='https://github.com/pierrebaudot/infotopopy/archive/0.1.tar.gz',
- 
-      license='BSD 3-Clause "New" or "Revised" License',
- 
-      author='Pierre Baudot',
- 
-      author_email='pierre.baudot@gmail.com',
- 
-      description='Manage configuration files',
- 
-      packages=find_packages(exclude=['tests']),
- 
-      long_description=open('README.md').read(),
- 
-      zip_safe=False)
+    long_description = fh.read()
+
+setuptools.setup(
+    name="infotopo", # Replace with your own username
+    version="0.1",
+    license='BSD 3-Clause "New" or "Revised" License',
+    author="Pierre Baudot",   
+    author_email="pierre.baudot@gmail.com",
+    description="InfoTopo: Topological Information Data Analysis. Deep statistical unsupervised and supervised learning.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pierrebaudot/infotopopy/archive/0.1.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
